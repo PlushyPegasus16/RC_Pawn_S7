0f87463259bcc132f844757f92f1702ceb7e8b32
Vincent <75223741+PlushyPegasus16@users.noreply.github.com>
2023-11-23 13:15:13 +0100

    Update RC-Pawn-Transmitter-Joystick.ino
    Updated comments. sending char-array with the xy-values of the joystick through the RF24L01 module. need to be decoded at the receivers end into Integers

--- RC-Pawn-Transmitter-Joystick.ino
+++ RC-Pawn-Transmitter-Joystick.ino
@@ -1,19 +1,20 @@
+//Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
 
 RF24 radio(8, 9); // CE, CSN
-const byte address[6] = "00001";
+const byte address[6] = "00001"; // byte array:pipeline address, to communicate.
 
-char xyData[32] = "";
+char xyData[32] = ""; //defining xy-data array
 String xAxis, yAxis;
 
 void setup() {
   Serial.begin(9600);
-  radio.begin();
-  radio.openWritingPipe(address);
-  radio.setPALevel(RF24_PA_MIN);
-  radio.stopListening();
+  radio.begin(); //start transmitter listening and receiving
+  radio.openWritingPipe(address); //setting address to write data through
+  radio.setPALevel(RF24_PA_MIN); //power amplifier level, how far the modules are away use capacitors for stable voltage.
+  radio.stopListening(); //stop transmitter listen and receiving 
 }
 
 void loop() {
@@ -24,7 +25,7 @@ void loop() {
   xAxis.toCharArray(xyData, 5); // Put the String (X Value) into a character array
   radio.write(&xyData, sizeof(xyData)); // Send the array data (X value) to the other NRF24L01 modile
   // Y value
-  yAxis.toCharArray(xyData, 5);
+  yAxis.toCharArray(xyData, 5); //put the String (x Value)into a character array
   radio.write(&xyData, sizeof(xyData));
   delay(20);
 }
